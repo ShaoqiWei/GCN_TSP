b7c8ce7056a9e84e07ecea1b8a2998fdaed774a8
ShaoqiWei <42299819+ShaoqiWei@users.noreply.github.com>
2023-04-13 15:25:38 +0900

    Add files via upload

--- network/Network.py
+++ network/Network.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from model.Layers import Encoder, Decoder
+from GCN_TSP.network.Layers import Encoder, Decoder
 
 
 class Transformer(nn.Module):
